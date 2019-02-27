03aae952e9fff2b284493e89abfb53b26275716a
Gajos <thegajos@gmail.com>
2019-02-27 22:21:59 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,17 @@
 Auth::routes();
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::middleware('auth')->group(function() {
+Route::middleware('auth')->group(function () {
 
     Route::view('/', 'empty')->name('home');
     Route::view('szukaj', 'empty')->name('szukaj');
 
-    Route::prefix('zlecenia')->name('zlecenia.')->group(function() {
+    Route::prefix('zlecenia')->name('zlecenia.')->group(function () {
         Route::get('/', 'ZlecenieController@index')->name('lista');
         Route::get('/{id}', 'ZlecenieController@show')->name('show');
     });
 
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('/users', 'AdminController@users')->name('users');
+    });
 });
