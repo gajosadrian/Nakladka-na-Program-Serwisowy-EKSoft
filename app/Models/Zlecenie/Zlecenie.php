2beadb62b4f1033b3b72e6d38dce5e902a42436b
Gajos <thegajos@gmail.com>
2020-08-22 14:27:56 +0200

    update

--- app/Models/Zlecenie/Zlecenie.php
+++ app/Models/Zlecenie/Zlecenie.php
@@ -633,32 +633,35 @@ class Zlecenie extends Model
     {
         $array = [];
 
-        if (!$this->is_termin and $this->dni_od_statusu >= 1 and in_array($this->status_id, [Status::GOTOWE_DO_WYJAZDU_ID, Status::NIE_ODBIERA_ID, Status::PONOWNA_WIZYTA_ID, Status::ZLECENIE_WPISANE_ID]))
+        if (!$this->is_termin and $this->dni_od_statusu >= 3 and $this->isAktywnyBlad(3) and in_array($this->status_id, [Status::GOTOWE_DO_WYJAZDU_ID, Status::NIE_ODBIERA_ID, Status::PONOWNA_WIZYTA_ID, Status::ZLECENIE_WPISANE_ID]))
             $array[] = 'Ustal termin';
 
-        if ($this->dni_od_statusu >= 2 and $this->isAktywnyBlad(2) and in_array($this->status_id, [Status::UMOWIONO_ID, Status::GOTOWE_DO_WYJAZDU_ID, Status::NA_WARSZTACIE_ID, Status::NIE_ODBIERA_ID, Status::PONOWNA_WIZYTA_ID, Status::PREAUTORYZACJA_ID]))
+        if ($this->dni_od_zakonczenia >= 2 and $this->isAktywnyBlad(2) and in_array($this->status_id, [Status::UMOWIONO_ID, Status::GOTOWE_DO_WYJAZDU_ID, Status::NA_WARSZTACIE_ID, Status::NIE_ODBIERA_ID, Status::PONOWNA_WIZYTA_ID, Status::PREAUTORYZACJA_ID]))
             $array[] = 'Zlecenie niezamknięte';
 
         if ($this->dni_od_statusu >= 1 and $this->isAktywnyBlad(1) and in_array($this->status_id, [Status::DO_POINFORMOWANIA_ID, Status::INFO_O_KOSZTACH_ID]))
             $array[] = 'Dzwonić do klienta';
 
-        if ($this->dni_od_statusu >= 1 and $this->isAktywnyBlad(1) and in_array($this->status_id, [Status::DO_ZAMOWIENIA_ID, Status::DO_WYCENY_ID, Status::DO_WYJASNIENIA_ID]))
+        if ($this->dni_od_statusu >= 1 and $this->isAktywnyBlad(1) and in_array($this->status_id, [Status::DO_ZAMOWIENIA_ID, Status::DO_WYCENY_ID]))
+            $array[] = 'Brak reakcji';
+
+        if ($this->dni_od_statusu >= 3 and $this->isAktywnyBlad(3) and in_array($this->status_id, [Status::DO_WYJASNIENIA_ID]))
             $array[] = 'Brak reakcji';
 
         if ($this->dni_od_statusu >= 3 and $this->isAktywnyBlad(3) and in_array($this->status_id, [Status::ZALICZKA_ID]))
-            $array[] = 'Czy jest zaliczka?';
+            $array[] = 'Zaliczka';
 
         if ($this->dni_od_statusu >= 1 and $this->isAktywnyBlad(1) and in_array($this->status_id, [Status::DZWONIC_PO_ODBIOR_ID]))
             $array[] = 'Dzwonić po odbiór';
 
-        if ($this->dni_od_statusu >= 2 and $this->isAktywnyBlad(2) and in_array($this->status_id, [Status::DO_ODBIORU_ID]))
+        if ($this->dni_od_statusu >= 7 and $this->isAktywnyBlad(7) and in_array($this->status_id, [Status::DO_ODBIORU_ID]))
             $array[] = 'Dzwonić po odbiór';
 
-        if ($this->dni_od_statusu >= 5 and $this->isAktywnyBlad(5) and in_array($this->status_id, [Status::DO_ROZLICZENIA_ID]))
+        if ($this->dni_od_statusu >= 7 and $this->isAktywnyBlad(7) and in_array($this->status_id, [Status::DO_ROZLICZENIA_ID]))
             $array[] = 'Nierozliczone';
 
-        if ($this->dni_od_statusu >= 4 and $this->isAktywnyBlad(4) and in_array($this->status_id, [Status::ZAMOWIONO_CZESC_ID]))
-            $array[] = 'Czy dotarła część?';
+        if ($this->dni_od_statusu >= 7 and $this->isAktywnyBlad(7) and in_array($this->status_id, [Status::ZAMOWIONO_CZESC_ID]))
+            $array[] = 'Co z częścią?';
 
         return $array;
     }
@@ -1049,6 +1052,12 @@ HTML;
 
         if ($user->technik_id) {
             $this->addLog(Log::TYPE_OPIS, $user->id, $opis);
+        } else {
+            $this->zatwierdzony_blad()->delete();
+
+            $zatwierdzony_blad = new ZatwierdzonyBlad;
+            $zatwierdzony_blad->status_id = $this->status_id;
+            $this->zatwierdzony_blad()->save($zatwierdzony_blad);
         }
     }
 
